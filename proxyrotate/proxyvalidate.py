67b3381c9f6fc942c95e85b7ac267f0a01181f54
0xdeadhead <hehe>
2021-05-14 16:42:29 +0530

    Initial Commit

---
+++ proxyrotate/proxyvalidate.py
@@ -0,0 +1,125 @@
+import asyncio
+import aiohttp
+import sys
+import re
+from termcolor import colored
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+
+# Queue for storing proxies
+
+
+class CircularQueue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, data):
+        if not data in self.queue:
+            self.queue.append(data)
+            return True
+        else:
+            return False
+
+    def dequeue(self):
+        if len(self.queue) == 0:
+            return None
+        else:
+            return self.queue.pop(0)
+
+    def display(self):
+        for element in self.queue:
+            print(element, file=sys.stderr)
+
+    def size(self):
+        return len(self.queue)
+
+# Extract proxies from sourcepage
+
+
+async def extractProxies(_url, _client, _proxies, _bar):
+    # _bar.write(colored(f"[*] Fetching proxy list from {_url}", color="cyan"))
+    async with _client.get(_url) as resp:
+        _pageSource = await resp.text()
+        if _url == "https://sslproxies.org/" or _url == "https://free-proxy-list.net/":
+            soup = BeautifulSoup(_pageSource, 'html.parser')
+            proxies_table = soup.find(id='proxylisttable')
+            for row in proxies_table.tbody.find_all('tr'):
+                _proxies.enqueue({
+                    'ip':   row.find_all('td')[0].string,
+                    'port': row.find_all('td')[1].string
+                })
+
+
+# Filtering Non-functional proxies
+
+
+async def filterProxies(_client, _proxies, _proxy, _rx4, _rx6, _sem, _retries, _pbar, _originIp=None):
+    while _retries != 0:
+        try:
+            await _sem.acquire()
+            async with _client.get("https://httpbin.org/ip", proxy=f"http://{_proxy['ip']}:{_proxy['port']}") as resp:
+                respjson = await resp.json()
+                respData = respjson["origin"]
+                _pbar.update()
+                _sem.release()
+                # Check if proxy discloses origin IP and skip adding the proxies
+                if _originIp in respData:
+                    _pbar.write(
+                        f"Transparent : {_proxy['ip']}:{_proxy['port']}", file=sys.stderr)
+                    break
+                # Check if Ip address is present in resp
+                if _rx4.search(respData) or _rx6.search(respData):
+                    _proxies.enqueue(_proxy)
+                    break
+        # Handling Timeout and other exceptions
+        except Exception:
+            _pbar.update()
+            _sem.release()
+        _retries -= 1
+
+# _existingProxies is used for only querying proxies which doesnt exist in current proxylist see line 92
+
+
+async def getProxyList(_timeout=15, _concurrency=120, _retries=2, bar=None, _existingProxies=CircularQueue()):
+    clientTimeout = aiohttp.ClientTimeout(total=_timeout)
+    # Set progress bar if it doesnt exists
+    if not bar:
+        bar = tqdm(desc="Validating Proxies", total=_retries*100, unit="req")
+    async with aiohttp.ClientSession(timeout=clientTimeout) as client:
+       # Fetching list of proxies from sources and extracting
+        sources = ["https://free-proxy-list.net/", "https://sslproxies.org/"]
+        proxies = CircularQueue()
+        proxyTasks = [extractProxies(url, client, proxies, bar)
+                      for url in sources]
+        await asyncio.gather(*proxyTasks)
+        # Querying for Origin Ip for removing transparent proxies
+        async with client.get("https://httpbin.org/ip") as resp:
+            respjson = await resp.json()
+            originIP = respjson["origin"]
+        no_of_reqs = proxies.size()*_retries
+        sem = asyncio.Semaphore(_concurrency)
+        # Regexes for IP adresses
+        rx4 = re.compile(r"^^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+        rx6 = re.compile(r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))")
+        tasks = []
+        proxy = proxies.dequeue()
+        # Creating Async Tasks for Ip verification
+        # reset total elements in bar
+        bar.total = 0
+        while proxy:
+            # check for existince of current proxy in valid proxies
+            if not proxy in _existingProxies.queue:
+                # update total items in progressbar i.e .. add no of retries to current
+                bar.total += _retries
+                tasks.append(filterProxies(client, proxies,
+                                           proxy, rx4, rx6, sem, _retries, bar, _originIp=originIP))
+            proxy = proxies.dequeue()
+        await asyncio.gather(*tasks)
+        # resetting Progressbar
+        bar.update(-bar.n)
+        return proxies
+
+
+if __name__ == "__main__":
+    proxy_q = asyncio.run(getProxyList(_timeout=20))
+    print(proxy_q.size())
---
+++ proxyrotate/update.py
@@ -0,0 +1,112 @@
+import asyncio
+import sys
+import os
+from proxyrotate import proxyvalidate
+from time import sleep
+from argparse import ArgumentParser
+from termcolor import colored
+from tqdm import tqdm
+from art import text2art
+
+
+# _delay in secs b/w validating next set of proxies,_timeout for proxy validation
+async def updateProxies(_proxies, _bar, _configDir, _delay=20, _timeout=15, _concurrency=120, _retries=2):
+    server_id = 0
+    # Terminate and restart haproxy
+    PROXY_STATE = "INACTIVE"
+    while True:
+        _newProxies = await proxyvalidate.getProxyList(
+            bar=_bar, _existingProxies=_proxies, _timeout=_timeout, _concurrency=_concurrency, _retries=_retries)
+        # Ensure only one thread accessing proxyQueue
+        _proxy = _newProxies.dequeue()
+        _newCount = 0
+        # reading old proxies
+        with open(f"{_configDir}/proxies.cfg") as config_file:
+            _lines = config_file.readlines()
+        # Appending new proxies to queue
+        with open(f"{_configDir}/proxies.cfg", "w") as config_file:
+            newProxieString = ""
+            while _proxy:
+                if _proxies.enqueue(_proxy):
+                    newProxieString += f"    server s{server_id} {_proxy['ip']}:{_proxy['port']}\n"
+                    server_id += 1
+                    _newCount += 1
+                _proxy = _newProxies.dequeue()
+            # Inserting new proxies at top for round robin support
+            _lines.insert(16, newProxieString)
+            config_file.write("".join(_lines))
+        if PROXY_STATE == "INACTIVE":
+            kill_flag = ""
+            PROXY_STATE = "ACTIVE"
+        else:
+            kill_flag = f"-sf $(cat {_configDir}/haproxy.pid)"
+        # restarting proxy
+        p = await asyncio.create_subprocess_shell(f"haproxy -D -f {_configDir}/proxies.cfg -p {_configDir}/haproxy.pid {kill_flag}", stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        out, err = await p.communicate()
+        if err:
+            _bar.write(colored(f"{err.decode()}", color="red"))
+        elif out:
+            _bar.write(colored(f"{out.decode()}", color="cyan"))
+        _bar.write(
+            colored(f"[*] Restarted haproxy with {_newCount} new proxies written to {_configDir}/proxies.cfg", color="cyan"))
+        sleep(_delay)
+
+
+def main():
+    parser = ArgumentParser()
+    print(text2art("proxyrotate", font="doubleshorts"), file=sys.stderr)
+    parser.add_argument(
+        "-d", "--delay", help="Cooldown b/w validating next set of proxies", default=20, type=int)
+    parser.add_argument("-t", "--timeout",
+                        help="Timeout for each proxy", default=15, type=int)
+    parser.add_argument("-c", "--concurrency",
+                        help="No of concurrent proxy validations", default=120, type=int)
+    parser.add_argument(
+        "-r", "--retries", help="No of retries before invalidating a proxy", default=2, type=int)
+    parser.add_argument("-A", "--alg", choices=['roundrobin', 'leastconn'],
+                        default="roundrobin", help="Load balancing algorithm")
+    parser.add_argument("-P", "--port", default=4444,
+                        help="port for binding haproxy frontend", type=int)
+    args = parser.parse_args()
+
+    DELAY, TIMEOUT, CONCURRENCY, RETRIES, ALG, PORT = args.delay, args.timeout, args.concurrency, args.retries, args.alg, args.port
+
+    # Initial Configuration
+    initial_doc = f'''global
+    daemon
+    maxconn 256
+
+    defaults
+    mode tcp
+    timeout connect 5000ms
+    timeout client 50000ms
+    timeout server 50000ms
+
+    frontend rotatingproxies
+    bind 127.0.0.1:{PORT}
+    default_backend proxy_servers
+
+    backend proxy_servers
+    balance {ALG}
+'''
+
+    try:
+        _configDir = '/tmp/proxyrotate'
+        os.system(f"mkdir -p {_configDir}")
+        # Initializing configuration file
+        with open(f"{_configDir}/proxies.cfg", "w") as config_file:
+            config_file.write(initial_doc)
+        with tqdm(desc=f"haproxy configuration: proxy_addr={colored('http://127.0.0.1:'+str(PORT),'white',attrs=['bold'])} algorithm={colored(ALG,'white',attrs=['bold'])}          Validating proxies", total=200, unit="req") as bar:
+            _proxies = proxyvalidate.CircularQueue()
+            asyncio.run(updateProxies(_proxies, bar, _configDir, _delay=DELAY,
+                        _timeout=TIMEOUT, _concurrency=CONCURRENCY, _retries=RETRIES))
+    except Exception as e:
+        print(colored(f"Exiting Reason {e}"))
+    except KeyboardInterrupt:
+        print(colored(f"Exiting"))
+    finally:
+        os.system(f"rm -r {_configDir} ; killall -9 haproxy")
+
+
+if __name__ == "__main__":
+    main()
